2013ad5b402e6b16896ed439b18ae10f74220066
Tam Nguyen <tam.nguyen@mercatus.com>
2025-03-16 14:04:51 +0700

    update add service Opentelemetry.

--- NDTCore.IdentityServer.Infrastructure/ServiceCollectionExtensions.cs
+++ NDTCore.IdentityServer.Infrastructure/ServiceCollectionExtensions.cs
@@ -2,10 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using NDTCore.IdentityServer.Contract;
+using NDTCore.IdentityServer.Domain.Constants;
 using NDTCore.IdentityServer.Domain.Entities;
 using NDTCore.IdentityServer.Infrastructure.Persistences;
 using NDTCore.IdentityServer.Infrastructure.Repositories;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 namespace NDTCore.IdentityServer.Infrastructure
 {
@@ -37,6 +44,54 @@ namespace NDTCore.IdentityServer.Infrastructure
                 })
                 .AddAspNetIdentity<AppUser>()
                 .AddTestUsers(TestUsers.Users);
+
+            return services;
+        }
+
+        public static IServiceCollection AddStackOpenTelemetry(this IServiceCollection services, IConfiguration configuration)
+        {
+            var resource = ResourceBuilder
+                            .CreateDefault()
+                            .AddService(OtelConstants.APP_OTEL_RESOURCE_SERVICE_NAME,
+                                        OtelConstants.APP_OTEL_RESOURCE_SERVICE_NAME,
+                                        OtelConstants.APP_OTEL_RESOURCE_SERVICE_VERSION);
+
+            services.AddLogging(logging => logging
+                    .AddOpenTelemetry(options => options
+                        .SetResourceBuilder(resource)
+                        .AddConsoleExporter()
+                        .AddOtlpExporter()
+                    )
+            );
+
+            services.AddOpenTelemetry()
+                    .WithTracing(tracerBuilder => tracerBuilder
+                        .SetResourceBuilder(resource)
+                        .AddAspNetCoreInstrumentation()
+                        .AddHttpClientInstrumentation()
+                        .AddSqlClientInstrumentation()
+                        .AddConsoleExporter()
+                        .AddOtlpExporter(options =>
+                        {
+                            // can get it in appsettings.json
+                            options.Endpoint = new Uri(OtelConstants.OTEL_EXPORTER_OTLP_ENDPOINT);
+                            options.Protocol = OtlpExportProtocol.HttpProtobuf;
+                        })
+                    );
+
+            services.AddOpenTelemetry()
+                    .WithMetrics(metricBuilder => metricBuilder
+                        .SetResourceBuilder(resource)
+                        .AddAspNetCoreInstrumentation()
+                        .AddHttpClientInstrumentation()
+                        .AddRuntimeInstrumentation()
+                        .AddConsoleExporter()
+                        .AddOtlpExporter(options =>
+                        {
+                            options.Endpoint = new Uri(OtelConstants.OTEL_EXPORTER_OTLP_ENDPOINT);
+                            options.Protocol = OtlpExportProtocol.HttpProtobuf;
+                        })
+                    );
 
             return services;
         }
